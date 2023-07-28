baa0f7868031c2c2e3a68186d97b87ebb1be34d5
Matthew <matthewb@uvic.ca>
2023-07-28 09:07:03 -0700

    Add ccsudo sudoers rules (#1)

---
+++ site/profile/manifests/ccsudo.pp
@@ -0,0 +1,8 @@
+class profile::ccsudo::sudoers {
+  file { '/etc/sudoers.d/99-ccsudo':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    content => '%cc_staff ALL=(root) NOPASSWD:/software/sbin/ccsudo'
+  }
+}
